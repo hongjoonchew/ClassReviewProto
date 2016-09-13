3355bf47930dc8d0ee0727b5939fb4a262c60e15
Stanley <laostan@gmail.com>
2016-09-12 21:40:31 -0700

    Reroutes
    homepage,about,index,help. localhost3000 redirects to homepage

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def home
+  end
+
+  def index
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'home/about'
+
+  get 'home/home'
+
+  get 'home/index'
+
+  get 'home/help'
+
+  #route to homepage
+  root 'reviews#index'
   resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
