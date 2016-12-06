669c22e6b0cecebd94c9198eddf832cb7ce10436
hongjoonchew <hongjoon.chew@gmail.com>
2016-12-05 19:57:45 -0800

    removed aws keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,13 +84,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.paperclip_defaults = {
+ config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('classify131'),
-    access_key_id: ENV.fetch('AKIAJKVYFRMQS4PNATUQ'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('us-west-1'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
 end
